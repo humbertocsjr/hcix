3b380b4e19f467f29da9d65a6f9d00ceb7049125
Humberto Costa <humberto.costa@me.com>
2025-03-02 20:26:45 -0300

    patuafs: fix disk_img.c

--- devkit/patuafs/disk_img.c
+++ devkit/patuafs/disk_img.c
@@ -5,16 +5,21 @@ static FILE *_disk;
 void disk_open(char *name)
 {
     _disk = fopen(name, "rb+");
+    if(!_disk)
+    {
+        fprintf(stderr, "error: can't open disk: %s\n", name);
+        exit(1);
+    }
 }
 
 int disk_read(uint32_t address, void *block)
 {
     fseek(_disk, (long)address * 1024, SEEK_SET);
-    fread(block, 1024, 1, _disk);
+    return fread(block, 1024, 1, _disk);
 }
 
 int disk_write(uint32_t address, void *block)
 {
     fseek(_disk, (long)address * 1024, SEEK_SET);
-    fread(block, 1024, 1, _disk);
+    return fwrite(block, 1024, 1, _disk);
 }
